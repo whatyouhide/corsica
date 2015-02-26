ea1067d46efeed71640741ade896a2bc8b3a0257
Andrea Leopardi <an.leopardi@gmail.com>
2015-02-26 17:13:49 +0100

    Prepare mix.exs for v0.1.0

--- mix.exs
+++ mix.exs
@@ -1,15 +1,29 @@
 defmodule Corsica.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
-    [app: :corsica,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     deps: deps]
+    [
+      app: :corsica,
+      version: @version,
+      elixir: "~> 1.0",
+      deps: deps,
+      description: description,
+      name: "Corsica",
+      source_url: "https://github.com/whatyouhide/corsica",
+      package: package,
+    ]
   end
 
   def application do
     [applications: [:logger, :cowboy, :plug]]
+  end
+
+  defp description do
+    """
+    Plug and DSL for efficiently handling or responding to CORS requests
+    """
   end
 
   defp deps do
@@ -18,6 +32,14 @@ defmodule Corsica.Mixfile do
       {:plug, ">= 0.9.0", optional: true},
       {:earmark, "~> 0.1", only: :docs},
       {:ex_doc, "~> 0.7", only: :docs}
+    ]
+  end
+
+  defp package do
+    [
+      contributors: "Andrea Leopardi",
+      license: ["MIT"],
+      links: %{"GitHub" => "https://github.com/whatyouhide/corsica"},
     ]
   end
 end
