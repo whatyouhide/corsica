61dbbaf0ef32d92d43e009037939c1212f192fd5
Andrea Leopardi <an.leopardi@gmail.com>
2016-03-25 10:27:59 +0100

    Adapt the code style in mix.exs to the current Elixir standards

--- mix.exs
+++ mix.exs
@@ -4,16 +4,14 @@ defmodule Corsica.Mixfile do
   @version "0.4.1"
 
   def project do
-    [
-      app: :corsica,
-      version: @version,
-      elixir: "~> 1.0",
-      deps: deps,
-      description: description,
-      name: "Corsica",
-      source_url: "https://github.com/whatyouhide/corsica",
-      package: package,
-    ]
+    [app: :corsica,
+     version: @version,
+     elixir: "~> 1.0",
+     deps: deps,
+     description: description,
+     name: "Corsica",
+     source_url: "https://github.com/whatyouhide/corsica",
+     package: package]
   end
 
   def application do
@@ -27,20 +25,16 @@ defmodule Corsica.Mixfile do
   end
 
   defp deps do
-    [
-      {:cowboy, ">= 1.0.0"},
-      {:plug, ">= 0.9.0"},
-      {:earmark, ">= 0.1.0", only: :docs},
-      {:ex_doc, "~> 0.9", only: :docs},
-      {:inch_ex, only: :docs},
-    ]
+    [{:cowboy, ">= 1.0.0"},
+     {:plug, ">= 0.9.0"},
+     {:earmark, ">= 0.1.0", only: :docs},
+     {:ex_doc, "~> 0.9", only: :docs},
+     {:inch_ex, only: :docs}]
   end
 
   defp package do
-    [
-      contributors: ["Andrea Leopardi"],
-      licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/whatyouhide/corsica"},
-    ]
+    [maintainers: ["Andrea Leopardi"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/whatyouhide/corsica"}]
   end
 end
