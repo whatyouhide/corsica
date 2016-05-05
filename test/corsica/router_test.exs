dbf099b7c91d0c40b63eb97446f64573579dc78a
Brightergy-SamarAcharya <samar.acharya@brightergy.com>
2016-05-05 06:43:44 -0500

    Escape Corsica.Router's options (#19)

--- test/corsica/router_test.exs
+++ test/corsica/router_test.exs
@@ -20,10 +20,34 @@ defmodule Corsica.RouterTest do
     get _, do: send_resp(conn, 200, "match")
   end
 
+  defmodule OriginRegexRouter do
+    use Corsica.Router, origins: [~r{http://.*\.foo.com}], max_age: 600
+
+    resource "/foo"
+    resource "/bar", origins: ~r/\.com$/
+    resource "/wild/*", origins: "*"
+    resource "/preflight", allow_methods: ~w(PUT)
+  end
+
+  defmodule OriginRegexPipeline do
+    use Plug.Router
+    plug OriginRegexRouter
+    plug :match
+    plug :dispatch
+
+    get _, do: send_resp(conn, 200, "regex")
+  end
+
   test "/foo" do
     conn = conn(:get, "/foo") |> put_origin("foo.com") |> Pipeline.call([])
     assert conn.resp_body == "match"
     assert get_resp_header(conn, "access-control-allow-origin") == ["*"]
+  end
+
+  test "/foo on origins as regex" do
+    conn = conn(:get, "/foo") |> put_origin("http://bar.foo.com") |> OriginRegexPipeline.call([])
+    assert conn.resp_body == "regex"
+    assert get_resp_header(conn, "access-control-allow-origin") == ["http://bar.foo.com"]
   end
 
   test "/bar" do
@@ -50,6 +74,17 @@ defmodule Corsica.RouterTest do
     assert conn.state == :sent
     assert conn.resp_body == ""
     assert get_resp_header(conn, "access-control-allow-origin") == ["*"]
+    assert get_resp_header(conn, "access-control-allow-methods") == ["PUT"]
+  end
+
+  test "preflight requests on origin as regex" do
+    conn = conn(:options, "/preflight")
+            |> put_origin("http://bar.foo.com")
+            |> put_req_header("access-control-request-method", "PUT")
+            |> OriginRegexPipeline.call([])
+    assert conn.state == :sent
+    assert conn.resp_body == ""
+    assert get_resp_header(conn, "access-control-allow-origin") == ["http://bar.foo.com"]
     assert get_resp_header(conn, "access-control-allow-methods") == ["PUT"]
   end
 
