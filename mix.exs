12d321c9048a6656ae4899d77d51e79336de9652
Andrea Leopardi <an.leopardi@gmail.com>
2017-08-15 18:04:47 +0200

    Add a page about common issues to the documentation
    [ci skip]
    
    Thanks to @yjukaku for the inspiration and first draft of contents
    provided in https://github.com/whatyouhide/corsica/pull/29.

--- mix.exs
+++ mix.exs
@@ -6,14 +6,25 @@ defmodule Corsica.Mixfile do
   @description "Plug-based swiss-army knife for CORS requests."
 
   def project() do
-    [app: :corsica,
-     version: @version,
-     elixir: "~> 1.3",
-     deps: deps(),
-     description: @description,
-     name: "Corsica",
-     source_url: "https://github.com/whatyouhide/corsica",
-     package: package()]
+    [
+      app: :corsica,
+      version: @version,
+      elixir: "~> 1.3",
+      deps: deps(),
+
+      # Hex
+      package: package(),
+      description: @description,
+
+      # Docs
+      name: "Corsica",
+      docs: [
+        main: "Corsica",
+        source_ref: "v#{@version}",
+        source_url: "https://github.com/whatyouhide/corsica",
+        extras: ["README.md", "pages/Common issues.md"],
+      ],
+    ]
   end
 
   def application() do
@@ -21,14 +32,18 @@ defmodule Corsica.Mixfile do
   end
 
   defp deps() do
-    [{:cowboy, "~> 1.0"},
-     {:plug, "~> 1.0"},
-     {:ex_doc, "~> 0.15", only: :dev}]
+    [
+      {:cowboy, "~> 1.0"},
+      {:plug, "~> 1.0"},
+      {:ex_doc, "~> 0.15", only: :dev},
+    ]
   end
 
   defp package() do
-    [maintainers: ["Andrea Leopardi"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/whatyouhide/corsica"}]
+    [
+      maintainers: ["Andrea Leopardi"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/whatyouhide/corsica"},
+    ]
   end
 end
