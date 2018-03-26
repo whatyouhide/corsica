10c6df9e588985b318b68f07819ed068c4300009
Christian Wesselhoeft <christian.wesselhoeft@gmail.com>
2018-03-26 11:00:54 -0600

    .formatter.exs: Export locals without parens

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,8 @@
 # Used by "mix format"
+export_locals_without_parens = [plug: :*, resource: :*, get: 2]
+
 [
   inputs: ["mix.exs", "{lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [plug: :*, resource: :*, get: 2]
+  locals_without_parens: export_locals_without_parens,
+  export: [locals_without_parens: export_locals_without_parens]
 ]
