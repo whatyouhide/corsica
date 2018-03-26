9afd6cfd2dddcc9d12751270bba2ea0dc0d67369
Christian Wesselhoeft <christian.wesselhoeft@gmail.com>
2018-03-26 11:02:17 -0600

    .formatter.exs: Only export `resource`

--- .formatter.exs
+++ .formatter.exs
@@ -1,8 +1,6 @@
 # Used by "mix format"
-export_locals_without_parens = [plug: :*, resource: :*, get: 2]
-
 [
   inputs: ["mix.exs", "{lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: export_locals_without_parens,
-  export: [locals_without_parens: export_locals_without_parens]
+  locals_without_parens: [plug: :*, resource: :*, get: 2],
+  export: [locals_without_parens: [resource: :*]]
 ]
