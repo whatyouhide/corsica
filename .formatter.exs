166a4552cd135c12be69d981cef11d8e6de99e92
Christian Wesselhoeft <christian.wesselhoeft@gmail.com>
2018-03-27 08:13:57 -0600

    .formatter.exs: Export explicit arities of `resource`

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,6 @@
 # Used by "mix format"
 [
   inputs: ["mix.exs", "{lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [plug: :*, resource: :*, get: 2],
-  export: [locals_without_parens: [resource: :*]]
+  locals_without_parens: [plug: :*, get: 2, resource: 1, resource: 2],
+  export: [locals_without_parens: [resource: 1, resource: 2]]
 ]
